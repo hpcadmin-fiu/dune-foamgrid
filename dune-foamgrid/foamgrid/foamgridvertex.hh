1e222a8b5e24144ec28ac34cc98004d9266db94a
sander@PCPOOL.MI.FU-BERLIN.DE <sander@PCPOOL.MI.FU-BERLIN.DE@10e313fe-3d75-4ea2-8e88-6fde81de6cbd>
2009-09-06 21:28:49 +0000

    remove unused constructor
    git-svn-id: https://svn.imp.fu-berlin.de/agnumpde/import-sander/trunk/dune-foamgrid@2748 10e313fe-3d75-4ea2-8e88-6fde81de6cbd

--- dune-foamgrid/foamgrid/foamgridvertex.hh
+++ dune-foamgrid/foamgrid/foamgridvertex.hh
@@ -12,11 +12,6 @@ namespace Dune {
     {
     public:
         
-        FoamGridEntityImp(int level, const Dune::FieldVector<double,dimworld>& pos) 
-            : pos_(pos), level_(level), 
-              son_(NULL)
-        {}
-        
         FoamGridEntityImp(int level, const FieldVector<double, dimworld>& pos, unsigned int id) 
             : pos_(pos), id_(id), level_(level), 
               son_(NULL) 
