6b13557738c48ccb9e2733275c2ef746fb196deb
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2015-06-15 15:26:03 +0200

    Add a flag helping to prevent double growth on the same codim 1 entity

--- dune/foamgrid/foamgrid/foamgridvertex.hh
+++ dune/foamgrid/foamgrid/foamgridvertex.hh
@@ -54,7 +54,7 @@ namespace Dune {
     public:
 
         FoamGridEntityImp(int level, const FieldVector<double, dimworld>& pos, unsigned int id)
-            : FoamGridEntityBase(level, id), pos_(pos), nSons_(0), elements_(), father_(nullptr)
+            : FoamGridEntityBase(level, id), pos_(pos), nSons_(0), elements_(), father_(nullptr), grew_(false)
         {
             sons_[0] = nullptr;
         }
@@ -131,6 +131,9 @@ namespace Dune {
 
         //! Son vertex on the next finer grid
         array<FoamGridEntityImp<0, dimgrid, dimworld>*, 1> sons_;
+
+        //! Flag if the facet has already caused a new element to grow
+        bool grew_;
     };
 
 }
