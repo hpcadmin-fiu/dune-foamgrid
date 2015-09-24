0c7cbbe1cad60aaa036c3f3bddc8a7500fd9e47c
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2015-09-24 14:02:16 +0200

    [test] Implement test inserting and removing an element from a simple 2d grid.

---
+++ dune/foamgrid/test/growth-test-2d.cc
@@ -0,0 +1,186 @@
+// -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set ts=8 sw=4 et sts=4:
+#include <config.h>
+
+#include <dune/grid/io/file/gmshreader.hh>
+
+#include <dune/foamgrid/foamgrid.hh>
+#include <dune/grid/common/gridinfo.hh>
+#include <dune/grid/io/file/vtk/vtkwriter.hh>
+#include <dune/grid/utility/structuredgridfactory.hh>
+#include <dune/grid/common/mcmgmapper.hh>
+
+#if DUNE_VERSION_NEWER(DUNE_GRID, 2, 4)
+#include <dune/grid/test/gridcheck.hh>
+#include <dune/grid/test/checkadaptation.hh>
+#else
+#include <dune/grid/test/gridcheck.cc>
+#include <dune/grid/test/checkadaptation.cc>
+#endif
+
+template <class Grid>
+void checkGridElementGrowth(Grid& grid)
+{
+  using namespace Dune;
+  enum { dimworld = Grid::dimensionworld };
+  enum { dim = Grid::dimension };
+
+  // the vertex mapper
+  Dune::LeafMultipleCodimMultipleGeomTypeMapper<Grid,Dune::MCMGVertexLayout> mapper(grid);
+
+  // let the top element grow
+  for (const auto& element : elements(grid.leafGridView()))
+    for (const auto& intersection : intersections(grid.leafGridView(), element))
+      if(intersection.boundary() && intersection.geometry().center()[1] > 0.5)
+      {
+        const Dune::ReferenceElement<double,dim>& refElement
+                    = Dune::ReferenceElements<double,dim>::general(element.type());
+        auto&& v0 = element.template subEntity<dim>(refElement.subEntity(intersection.indexInInside(), dim-1, 0, dim));
+        auto&& v1 = element.template subEntity<dim>(refElement.subEntity(intersection.indexInInside(), dim-1, 1, dim));
+
+        // calculate new vertex position
+        Dune::FieldVector<double, dimworld> newVertex = v0.geometry().center();
+        newVertex += v1.geometry().center(); newVertex /= 2.0;
+        newVertex[1] += 0.5;
+
+        // insert new vertex
+        std::size_t vIdx = grid.insertVertex(newVertex);
+
+        // compile all vertex indices of the new element
+        std::vector<std::size_t> vertices = {mapper.index(v0), mapper.index(v1), vIdx};
+
+        // insert the element
+        grid.insertElement(element.type(), vertices);
+      }
+
+  std::size_t numBoundarySegments = grid.numBoundarySegments();
+  std::cout << std::endl<< "numBoundarySegments before growth: " << numBoundarySegments << std::endl;
+  std::cout << "-------------------------------------------" << std::endl;
+
+  bool elementsWillVanish = grid.preGrow();
+  if(elementsWillVanish)
+    DUNE_THROW(InvalidStateException,"grid.preGrow() does not return correct information");
+
+  // check mightVanish
+  for (const auto& element : elements(grid.levelGridView(0)))
+    checkHierarchy(element);
+
+  bool newElementGenerated = grid.grow();
+  if(!newElementGenerated)
+    DUNE_THROW(InvalidStateException,"grid.grow() does not return correct information");
+
+  grid.postGrow();
+
+  // Loop over all levels except the lowest one
+  for (int level = 0 ; level <= grid.maxLevel(); ++level )
+  {
+    for (const auto& element : elements(grid.levelGridView(level)))
+      if(element.isNew())
+        DUNE_THROW(InvalidStateException,"After postGrow() was called no entity is new, i.e., isNew() == false");
+  }
+
+  std::cout << "Boundary intersections after growth: " << std::endl;
+  int isCounter = 0;
+  for (const auto& element : elements(grid.leafGridView()))
+  {
+    for (const auto& intersection : intersections(grid.leafGridView(), element))
+    {
+      if(intersection.boundary())
+      {
+        std::cout << "Boundary Intersection no"<<isCounter<<" has segment index: " << intersection.boundarySegmentIndex() << std::endl;
+        ++isCounter;
+      }
+    }
+  }
+
+  numBoundarySegments = grid.numBoundarySegments();
+  std::cout << "-------------------------------------------" << std::endl;
+  std::cout << "numBoundarySegments after growth: " << numBoundarySegments << std::endl<< std::endl;
+}
+
+template <class Grid>
+void checkGridElementRemoval(Grid& grid)
+{
+  using namespace Dune;
+  enum { dimworld = Grid::dimensionworld };
+  enum { dim = Grid::dimension };
+
+  // remove the first inserted element
+  auto eIt = grid.leafGridView().template begin<0>();
+  grid.removeElement(*eIt);
+
+  bool elementsWillVanish = grid.preGrow();
+  if(!elementsWillVanish)
+    DUNE_THROW(InvalidStateException,"grid.preGrow() does not return correct information");
+
+  std::size_t numBoundarySegments = grid.numBoundarySegments();
+  std::cout << std::endl << "numBoundarySegments before removal: " << numBoundarySegments << std::endl;
+  std::cout << "-------------------------------------------" << std::endl;
+
+  bool newElementInserted = grid.grow();
+  if(newElementInserted)
+    DUNE_THROW(Dune::InvalidStateException,"grid.grow() does not return correct information");
+  grid.postGrow();
+
+  std::cout << "Boundary intersections after removal: " << std::endl;
+
+  int isCounter = 0;
+  for (const auto& element : elements(grid.leafGridView()))
+  {
+    for (const auto& intersection : intersections(grid.leafGridView(), element))
+    {
+      if(intersection.boundary())
+      {
+        std::cout << "Boundary Intersection no"<<isCounter<<" has segment index: " << intersection.boundarySegmentIndex() << std::endl;
+        ++isCounter;
+      }
+    }
+  }
+
+  numBoundarySegments = grid.numBoundarySegments();
+  std::cout << "-------------------------------------------" << std::endl;
+  std::cout << "numBoundarySegments after removal: " << numBoundarySegments << std::endl << std::endl;
+}
+
+using namespace Dune;
+
+int main (int argc, char *argv[])
+{
+  try
+  {
+    static const int dimworld = 3;
+    static const int dim = 2;
+    std::cout << "Creating a FoamGrid<"<<dim<<", "<<dimworld<<"> ("<<dim<<"d in "<<dimworld<<"d grid)" << std::endl;
+    Dune::FieldVector<double,dimworld> lower(0.0);
+    Dune::FieldVector<double,dimworld> upper(1.0);
+    std::array<unsigned int,dim> elements;
+    std::fill(elements.begin(), elements.end(), 1);
+    std::shared_ptr<FoamGrid<dim,dimworld> > grid = StructuredGridFactory<FoamGrid<dim,dimworld> >::createSimplexGrid(lower,upper,elements);
+
+    Dune::VTKWriter<typename FoamGrid<dim,dimworld>::LeafGridView > writer(grid->leafGridView(), VTK::nonconforming);
+    writer.write("before_growth");
+
+    // check simple grid growth
+    Dune::gridinfo(*grid);
+    checkGridElementGrowth(*grid);
+    writer.write("after_growth");
+    Dune::gridinfo(*grid);
+
+    // check removal of a grid element
+    checkGridElementRemoval(*grid);
+    writer.write("after_removal");
+    Dune::gridinfo(*grid);
+
+    // do a grid check on a refined grid
+    grid->globalRefine(4);
+    gridcheck(*grid);
+  }
+  // //////////////////////////////////
+  //   Error handler
+  // /////////////////////////////////
+  catch (Exception e) {
+    std::cout << e << std::endl;
+    return 1;
+  }
+  return 0;
+}
