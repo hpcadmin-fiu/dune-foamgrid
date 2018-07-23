cf2ed5af500feab897aadd1f431a3d8eb4cc240f
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2018-07-23 15:23:57 +0000

    [!42] [vertex] Initialize vertex_ member. Fixes compiler warning.
    Merge branch 'fix/initialize-vertex-member' into 'master'
    
    See merge request [extensions/dune-foamgrid!42]
    
      [extensions/dune-foamgrid!42]: Noneextensions/dune-foamgrid/merge_requests/42
    
    
    (cherry picked from commit 44a0b570ddb60cc3fac577ed0bd918114ce2ce9a)
    
    dc455b4a [vertex] Initialize vertex_ member. Fixes compiler warning.

--- dune/foamgrid/foamgrid/foamgridvertex.hh
+++ dune/foamgrid/foamgrid/foamgridvertex.hh
@@ -123,7 +123,7 @@ namespace Dune {
         std::vector<const FoamGridEntityImp<dimgrid, dimgrid ,dimworld>*> elements_;
 
         //! A vertex array for compatibility reasons with edges. Calling results in undefined behaviour.
-        std::array<const FoamGridEntityImp<0, dimgrid ,dimworld>*, 1> vertex_;
+        std::array<const FoamGridEntityImp<0, dimgrid ,dimworld>*, 1> vertex_ = {{ nullptr }};
 
         //! Boundary index if vertex is on boundary
         //  only used if the vertex is a boundary vertex
